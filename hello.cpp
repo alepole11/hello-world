ac4db7ce020eded2182f624fb0e2761f4ece5598
alepole11 <22509863+alepole11@users.noreply.github.com>
2025-09-10 22:50:42 +0200

    Aggiunta domanda biscotto fortuna
    Inserita una domanda su biscotto fortuna, con risposta fissa in base a SI / NO

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main(){
-  cout<<"Hello World";
+  cout<<"Hello World"<<endl;
+  cout<<"Vuoi scartare un biscotto della fortuna? ";
+  string risposta;
+  cin>>risposta;
+  if(risposta == "SI" || risposta == "si" || risposta == "Si"){
+	cout<<endl<<"Se cent'anni campare vuoi, fatti sempre i fatti tuoi";
+  }else{
+	cout<<endl<<"No problem, arrivederci";
+  }
 }
