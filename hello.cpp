29e777e2ae4621c4cefa3a7cc6156a1718a1be99
alepole11 <22509863+alepole11@users.noreply.github.com>
2025-09-10 22:57:30 +0200

    Aggiunta somma o sottrazione
    Chiede due numeri e quale operazione fare, per poi stampare risultato

--- hello.cpp
+++ hello.cpp
@@ -2,4 +2,18 @@
 using namespace std;
 int main(){
   cout<<"Hello World";
+  cout<<"Digita due numeri separati da spazio, quindi premi INVIO"<<endl;
+  int n1, n2;
+  cin>>n1>>n2;
+  cout<<"Scegli quale operazione svolgere: \n1. Somma\n2. Sottrazione\nScelta: ";
+  int risposta;
+  cin>>risposta;
+  if(risposta == 1){
+	cout<<"Somma: "<<n1+n2;
+  }else if(risposta == 2){
+	cout<<"Sottrazione: "<<n1-n2;
+  }else{
+	cout<<"Scelta non valida.";
+  }
+  
 }
